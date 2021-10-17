9d69be8d17e95d6f46c61b8ab3074d99f0da348a
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-10-17 20:08:11 +0100

    dashboard component implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
 
-function App (props) {
-  useEffect(()=> {
+function App(props) {
+  useEffect(() => {
     props.dispatch(handleInitialData());
-  })
+  });
 
-  return <div>Starter Code</div>;
+  return <div>{props.loading === true ? null : <Dashboard />}</div>;
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
